d825baffd038af8e63b93905243fa3324bc9163d
Christiaan Knaap <2102470+cknaap@users.noreply.github.com>
2022-04-11 07:21:29 +0200

    Powershell scripts

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿Push-Location $PSScriptRoot
+
+$VERSION = Get-Content -Path ./VERSION
+$VERSION
+
+# Build synthea distribution tar file
+./build_synthea.ps1
+
+# Containerize it
+docker build -t firely/synthea:$VERSION .
+
+Pop-Location
---
+++ build_synthea.ps1
@@ -0,0 +1,30 @@
+﻿echo "# "
+echo "# BEGIN OF BUILD SYNTHEA FROM $PSScriptRoot"
+echo "# "
+
+Push-Location $PSScriptRoot
+
+Remove-Item -Path ./synthea -Force -Recurse -ErrorAction SilentlyContinue
+
+$VERSION = Get-Content -Path ./VERSION
+$VERSION
+
+git clone --depth 1 --branch $VERSION https://github.com/synthetichealth/synthea.git
+
+mkdir ./synthea/.gradle
+docker run -u gradle -v ${PWD}/synthea:/home/gradle/project -w /home/gradle/project gradle:latest gradle assemble
+
+Remove-Item -Path ./synthea_dist -Force -Recurse -ErrorAction SilentlyContinue
+mkdir ./synthea_dist
+cp ./synthea/build/distributions/synthea.tar ./synthea_dist
+
+
+ls -l
+ls -l ./synthea_dist
+
+Remove-Item -Path ./synthea -Force -Recurse
+
+echo "# "
+echo "# END OF BUILD SYNTHEA"
+echo "# "
+Pop-Location
